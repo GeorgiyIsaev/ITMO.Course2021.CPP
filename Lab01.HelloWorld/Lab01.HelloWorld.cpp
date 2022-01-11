9ca7a8ead24b71bc0c305738eeedc89e25b49257
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-11 22:38:36 +0300

    fix

--- Lab01.HelloWorld/Lab01.HelloWorld.cpp
+++ Lab01.HelloWorld/Lab01.HelloWorld.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 void InOutTest() {
-    /*Упражнение 1. Ввод-вывод данных*/
+    /*Упражнение 1. Ввод-вывод данных */
     string name;
     cout << "What is your name? ";
     cin >> name;
-    cout << "Hello, " << name << "!\n";
+    cout << "Hello, " << name << "!" << endl;
 
     string surname;
     cout << "What is your surname? ";
@@ -17,13 +17,17 @@ void InOutTest() {
 }
 
 void ConvertType() {
-    /*Упражнение 2. Преобразование типов данных*/
+    /*Упражнение 2. Преобразование типов данных */
     double x;
     double a, b; cout << "\nВведите a и b:\n";
     cin >> a; // ввод с клавиатуры значения a
     cin >> b; // ввод с клавиатуры значения b
     x = a / b; // вычисление значения x
-    cout << "\nx = "
+    cout.precision(3);
+    cout<< "\nx = "<< x << endl;
+    cout << sizeof(a / b) << ends << sizeof(x) << endl;
+
+
 }
 
 int main()
@@ -32,7 +36,7 @@ int main()
     SetConsoleOutputCP(1251); //поддержка кипиллицы (способ 2)
     SetConsoleCP(1251);
     
-    InOutTest();
+    //InOutTest();
     ConvertType();
 
 
