364f7053ece3f03001a5baee4fe08a4cf4122740
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-11 22:28:04 +0300

    in out

--- Lab01.HelloWorld/Lab01.HelloWorld.cpp
+++ Lab01.HelloWorld/Lab01.HelloWorld.cpp
@@ -1,10 +1,21 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <string>
+using namespace std;
 
 int main()
 {
    // system("chcp 1251");  //поддержки кириллицы (способ 1)
     SetConsoleOutputCP(1251); //поддержка кипиллицы (способ 2)
     SetConsoleCP(1251);
-    std::cout << "Hello World!\n";
+    
+    string name;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Hello, " << name << "!\n";
+
+    string surname;
+    cout << "What is your surname? ";
+    getline(cin, surname);
+    cout << "Hello, " << surname <<"" << name << "!\n";
 }
