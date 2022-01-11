dcfdb230b01efccf598638878c972afd19bcd0c4
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-11 22:25:53 +0300

    kirillit

--- Lab01.HelloWorld/Lab01.HelloWorld.cpp
+++ Lab01.HelloWorld/Lab01.HelloWorld.cpp
@@ -1,6 +1,10 @@
 ﻿#include <iostream>
+#include <windows.h>
 
 int main()
 {
+   // system("chcp 1251");  //поддержки кириллицы (способ 1)
+    SetConsoleOutputCP(1251); //поддержка кипиллицы (способ 2)
+    SetConsoleCP(1251);
     std::cout << "Hello World!\n";
 }
