e5206a084865cacb284647db3e6819d32f32be2c
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-11 22:31:07 +0300

    ConvertType()

--- Lab01.HelloWorld/Lab01.HelloWorld.cpp
+++ Lab01.HelloWorld/Lab01.HelloWorld.cpp
@@ -3,12 +3,8 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-   // system("chcp 1251");  //поддержки кириллицы (способ 1)
-    SetConsoleOutputCP(1251); //поддержка кипиллицы (способ 2)
-    SetConsoleCP(1251);
-    
+void InOutTest() {
+    /*Упражнение 1. Ввод-вывод данных*/
     string name;
     cout << "What is your name? ";
     cin >> name;
@@ -17,5 +13,27 @@ int main()
     string surname;
     cout << "What is your surname? ";
     getline(cin, surname);
-    cout << "Hello, " << surname <<"" << name << "!\n";
+    cout << "Hello, " << surname << "" << name << "!\n";
+}
+
+void ConvertType() {
+    /*Упражнение 2. Преобразование типов данных*/
+    double x;
+    double a, b; cout << "\nВведите a и b:\n";
+    cin >> a; // ввод с клавиатуры значения a
+    cin >> b; // ввод с клавиатуры значения b
+    x = a / b; // вычисление значения x
+    cout << "\nx = "
+}
+
+int main()
+{
+   // system("chcp 1251");  //поддержки кириллицы (способ 1)
+    SetConsoleOutputCP(1251); //поддержка кипиллицы (способ 2)
+    SetConsoleCP(1251);
+    
+    InOutTest();
+    ConvertType();
+
+
 }
