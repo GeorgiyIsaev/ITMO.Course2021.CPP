6609235fbbd0a834cabc17248e2416cc480e0f06
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-12 22:18:25 +0300

    колличество точек

--- Lab01.HelloWorld/Lav01.Gauss.cpp
+++ Lab01.HelloWorld/Lav01.Gauss.cpp
@@ -5,10 +5,34 @@
 #include <string>
 #include <cmath>
 
+struct Point {
+	int x;
+	int y;
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+};
+
+int getSize() {
+	while (true) {
+		int size = 0;
+		std::cout << "Введите колличество углов >> ";
+		std::cin >> size;
+		if(size>0)
+			return 0;
+		std::cout << "ERROR: колличество углов не может быть отрицательным!   ";
+	}
+}
 
 
 void AreaPolygon() {
 	/*Задание 1. Расчет площади многоугольника*/
+
+	int size = getSize();
+	Point[] ponts = getpoints();
+
+
 
 
 }
