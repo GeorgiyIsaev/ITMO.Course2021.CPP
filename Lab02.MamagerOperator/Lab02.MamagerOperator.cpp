84112a8ea1da15a6222acc9484dbd026dd79ba0e
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-14 02:20:12 +0300

    fix

--- Lab02.MamagerOperator/Lab02.MamagerOperator.cpp
+++ Lab02.MamagerOperator/Lab02.MamagerOperator.cpp
@@ -8,9 +8,13 @@ int main()
     SetConsoleOutputCP(1251);
     SetConsoleCP(1251);
     /*Упражнение 1. Реализация операторов выбора*/
+    /*Задание 1. Определение принадлежности точки заданной фигуре*/
     ValidPoint(2, 2);
     ValidPoint(0, 0);
     ValidPoint(0, 3);
     ValidPoint(5, 3);
+    /*Задание 2. Использование оператора switch при реализации выбора*/
+    SwitchUse();
+    SwitchUse02();
 }
 
