ee7dc397b680925d03fc48a3c4c30a842ef7c15c
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-14 02:49:25 +0300

    Упражнение 4. Расчет суммы чисел на заданном интервале

--- Lab02.MamagerOperator/Lab.h
+++ Lab02.MamagerOperator/Lab.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <ctime>
 
 
 void ValidPoint(int x = 2, int y = 2) {
@@ -98,4 +99,39 @@ void WhileUse() {
 			b -= a;
 	}
 	std::cout << "НОД = " << a << std::endl;
+}
+
+void ForUse() {
+	/*Упражнение 3. Применение цикла с параметром (for) для итерации*/
+	srand(time(NULL));
+	int a, b, c;
+	int k = 0, n = 10;
+	for (int i = 1; i <= n; i++) {
+		// инициализация операндов случайными числами от 1 до 101 
+		a = rand() % 10 + 1;
+		b = rand() % 10 + 1;
+		std::cout << a << " * " << b << " = ";
+		std::cin >> c;
+		if (a * b != c)
+		{
+			k++; // операция «инкремент», аналогично: k = k + 1
+			std::cout << "Error! ";
+			std::cout << a << " * " << b << " = " << a * b << std::endl;
+		}
+	}
+	std::cout << "Count error: " << k << std::endl;
+}
+
+
+void SumOnInterval(int k = 1, int m = 99 ) {
+	/*Упражнение 4. Расчет суммы чисел на заданном интервале*/
+	/*для i, находящихся от 1 до k и от m до 100.*/
+	int sum = 0;
+	for (int i = 1; i <= 100; i++)
+	{
+		if ((i > k) && (i < m))
+			continue;
+		sum += i;
+	}
+	std::cout << "sum: " << sum << std::endl;
 }
