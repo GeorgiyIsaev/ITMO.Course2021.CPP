0191e9794953a8effd3c962a91f7884878b124ce
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-14 02:24:43 +0300

    Использование цикла с постусловием

--- Lab02.MamagerOperator/Lab.h
+++ Lab02.MamagerOperator/Lab.h
@@ -64,3 +64,20 @@ void SwitchUse02() {
 		std::cout << "Мощный двигатель\n";
 	}
 }
+
+void WhileUse() {
+	/*Упражнение 2. Использование циклов при реализации алгоритмов*/
+    /*Задание 1. Использование цикла с постусловием*/
+	double x, x1, x2, y;
+
+	std::cout << "x1 = "; std::cin >> x1;
+	std::cout << "x2 = "; std::cin >> x2;
+	std::cout << "\tx\tsin(x)\n";
+	x = x1;
+	do
+	{
+		y = sin(x);
+		std::cout << "\t" << x << "\t" << y << std::endl;
+		x = x + 0.01;
+	} while (x <= x2);
+}
