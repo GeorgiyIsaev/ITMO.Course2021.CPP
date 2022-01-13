eca4c2faf0f99914a46cdfacc9cff2208035e09a
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-13 21:52:00 +0300

    Задача вынесена в отдельный проект

--- Lab01.Gauss/Lab01.Gauss.cpp
+++ Lab01.Gauss/Lab01.Gauss.cpp
@@ -1,8 +1,70 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+struct Point {
+	int x;
+	int y;
+};
+
+/*Задание 1. Расчет площади многоугольника*/
+
+
+int getSize() {
+	while (true) {
+		int size = 0;
+		std::cout << "Введите колличество углов >> ";
+		std::cin >> size;
+		if (size > 0)
+			return size;
+		std::cout << "ERROR: колличество углов не может быть отрицательным!   ";
+	}
+	return 0;
+}
+
+Point getPoint(int i) {
+	std::cout << "Введите x и y для точки " << i << " >> ";
+	Point point;
+	std::cin >> point.x >> point.y;
+	return point;
+}
+
+Point* getPoints(const int size) {
+	Point* ponts = new Point[size];
+
+	for (int i=0; i < size; i++) {
+		ponts[i] = getPoint(i);
+	}
+	return ponts;
+}
+
+void printPonts(const int size, Point* ponts) {
+	std::cout << "Многоугольник из " << size << " углов.\n";
+	for (int i=0; i < size; i++) {
+		std::cout << "Угол " << i <<
+			": {" << ponts[i].x << ", "
+			<< ponts[i].y << "}\n";
+	}
+	std::cout << std::endl;
+}
+
+
+void AreaPolygon() {
+	/*Задание 1. Расчет площади многоугольника*/
+
+	int size = getSize();
+	Point* ponts = getPoints(size);
+	printPonts(size, ponts);
+
+	delete[] ponts;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SetConsoleOutputCP(1251); 
+	SetConsoleCP(1251);
+   	AreaPolygon();
 }
 
