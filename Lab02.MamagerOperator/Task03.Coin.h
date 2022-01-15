e5eea89ca9266bb05c2a8b7a95a3275e69d354d6
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-15 15:54:38 +0300

    task02

--- Lab02.MamagerOperator/Task03.Coin.h
+++ Lab02.MamagerOperator/Task03.Coin.h
@@ -8,17 +8,20 @@
 Каждое число из набора можно использовать сколько угодно раз.*/
 
 void Coin() {
-	int yuorCoin = 256;
+	int yourCoin = 256;
 	const int countTypeMonet = 4;
 	int currentMoneta[countTypeMonet] = { 10,5,2,1 };
-	int countMonets[countTypeMonet] = { 10,5,2,1 };
+	int countMonets[countTypeMonet];
+
 	for (int i = 0; i < countTypeMonet; i++) {
-		int module = yuorCoin % currentMoneta[i];
-		std::cout << module << std::endl;
+		int module = yourCoin % currentMoneta[i];
+		yourCoin = yourCoin - module;
+		countMonets[i] = yourCoin / currentMoneta[i];
+		yourCoin = module;
 	}
 
-
-
-
-
+	std::cout << "Размен для " << yourCoin << ":\n";
+	for (int i = 0; i < countTypeMonet; i++) {
+		std::cout << "Монет номиналом " << currentMoneta[i] << ": " << countMonets[i] << std::endl;
+	}
 }
