4be02f28783dcfab2f274d6f6fc758e158e561ce
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-15 16:01:02 +0300

    метод для калькуляции

--- Lab02.MamagerOperator/Task03.Coin.h
+++ Lab02.MamagerOperator/Task03.Coin.h
@@ -7,18 +7,40 @@
 Требуется выдать набор монет с наименьшим возможным количеством монет.
 Каждое число из набора можно использовать сколько угодно раз.*/
 
-void Coin() {
-	int yourCoin = 256;
-	const int countTypeMonet = 4;
-	int currentMoneta[countTypeMonet] = { 10,5,2,1 };
-	int countMonets[countTypeMonet];
+int getYourCoin() {
+	std::cout << "Введите колличество денег >> ";
+	std::string textYourCoin;
+	std::cin >> textYourCoin;
+	int yourCoin = 0;
+	try {
+		yourCoin = stoi(textYourCoin);
+	}
+	catch (int ex)
+	{
+		std::cout << "ERROR: Введеное значение не число! ";
+		getYear();
+	}
+	return yourCoin;
+}
 
+int* CalculationCountMonets(const int countTypeMonet, int* currentMoneta, int yourCoin) {
+	int *countMonets = new int[countTypeMonet];
 	for (int i = 0; i < countTypeMonet; i++) {
 		int module = yourCoin % currentMoneta[i];
 		yourCoin = yourCoin - module;
 		countMonets[i] = yourCoin / currentMoneta[i];
 		yourCoin = module;
 	}
+	return countMonets;
+}
+
+void Coin() {
+	int yourCoin = getYourCoin();
+	const int countTypeMonet = 4;
+	int currentMoneta[countTypeMonet] = { 10,5,2,1 };
+	int *countMonets = CalculationCountMonets(countTypeMonet, currentMoneta, yourCoin);
+
+
 
 	std::cout << "Размен для " << yourCoin << ":\n";
 	for (int i = 0; i < countTypeMonet; i++) {
