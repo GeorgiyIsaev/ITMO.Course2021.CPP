37e967542e775396b0d7ca61b48835c6eee2c81e
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-16 19:31:54 +0300

    Lab03

---
+++ Lab03.MyFunction/Lab03.MyFunction.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+#include "Lab03.h"
+
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	/*Лабороторная работа 03 */
+	mainLab03();
+
+}
+   
+
---
+++ Lab03.MyFunction/Lab03.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+void mainLab03() {
+
+}
+
