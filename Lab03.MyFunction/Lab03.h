bdcdb06c385e7790051e1849fbb5a1e4e6606f73
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-16 19:34:26 +0300

    t01

--- Lab03.MyFunction/Lab03.h
+++ Lab03.MyFunction/Lab03.h
@@ -1,10 +1,21 @@
 #pragma once
 #include <iostream>
-#include <windows.h>
 #include <string>
 #include <cmath>
 
+void privet(std::string name)
+{
+	std::cout << name << ", " << "hello!" << std::endl;
+}
+
+
+
 void mainLab03() {
+	/*Задание 1. Реализация процедуры*/
+	std::string name = "Чупакбра";
+	privet(name);
+
+
 
 }
 
