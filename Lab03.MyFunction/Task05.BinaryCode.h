a9e4aa6a874d1934fa1eb869097f023480986ba0
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-19 23:59:02 +0300

    full task05

--- Lab03.MyFunction/Task05.BinaryCode.h
+++ Lab03.MyFunction/Task05.BinaryCode.h
@@ -10,6 +10,18 @@
 выделять остаток с помощью выражения num%2.*/
 
 
-void TestBinaryCode() {
+int ConvertToBinary(int num)
+{
+    if (num < 2)
+        return num;
+    else
+        return (10 * ConvertToBinary(num / 2) + num % 2);
+}
 
+
+void TestBinaryCode() {
+    int input = GetValue("Введите число: ");
+    long inBinary = ConvertToBinary(input);
+    std::cout  << "Бинарное значение для числа " << input << " : " << inBinary << "\n";
+ 
 }
