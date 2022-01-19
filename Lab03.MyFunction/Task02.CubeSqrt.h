2717c6235273a76e40059f1558abc93de85506eb
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-19 22:53:05 +0300

    test1

--- Lab03.MyFunction/Task02.CubeSqrt.h
+++ Lab03.MyFunction/Task02.CubeSqrt.h
@@ -9,6 +9,28 @@
 
 
 
-void CubeSqrt() {
 
+double WhileCubeSqrt(double sq) {
+    double x = 0, // начальное значение x
+        x1 = sq; // значение x-1
+
+    while (true)
+    {
+        x = (1 / 3) * ((sq / (x1 * x1)) + (2 * x1));
+        if (abs(x1 - x) < pow(10.0, -5.0)) //
+        {
+            // cout << pow(10.0, -5.0);
+            break;
+        }
+    }
+    return x;
+}
+
+
+void CubeSqrt() {
+    double x = 5; //getValue("Введите значение под корнем"); 
+    double sq = WhileCubeSqrt(x);
+    double sq2 = pow(x, 1.0 / 3);
+
+    std::cout << sq << " " << sq2 << std::endl;
 }
