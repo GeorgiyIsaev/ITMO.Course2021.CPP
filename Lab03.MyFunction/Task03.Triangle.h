00687be2b6071eefd768048a6f385935aebf6347
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-19 23:29:08 +0300

    task3 test

--- Lab03.MyFunction/Task03.Triangle.h
+++ Lab03.MyFunction/Task03.Triangle.h
@@ -12,6 +12,51 @@
 Выбор типа треугольника реализуйте с помощью диалога с пользователем.*/
 
 
-void TriangleTest() {
+double calculateTriangleArea(double side) { 
+    double halfPerim = (side * 3) / 2;
+    return sqrt((halfPerim * (halfPerim - side) * (halfPerim - side) * (halfPerim - side)));
+}
 
+double calculateTriangleArea(double side, double side2, double side3) { 
+    double halfPerim = (side + side2 + side3) / 2;
+    return sqrt((halfPerim * (halfPerim - side) * (halfPerim - side2) * (halfPerim - side3)));
+}
+
+
+double EquilateralTriangleMenu() {
+    //для равносторнего треугольника
+    double side = 5;
+    double area = calculateTriangleArea(side);
+    return area;
+}
+double VersatileTriangleMenu() {
+    //для разносторонеего треугольника
+    double side1 = 5;
+    double side2 = 6;
+    double side3 = 4;
+    double area = calculateTriangleArea(side1, side2, side3);
+    return area;
+}
+
+
+void TriangleMenu() {
+    while (true) {
+        std::cout << "МЕНЮ:\n";
+        std::cout << "1-Равносторонний треугольник\n";
+        std::cout << "2-РАЗНОсторонний треугольник\n";
+        std::cout << "0-Выход\n>>";
+        std::string str = "";
+        std::cin >> str;
+        if (str == "0") {
+            break;
+        }
+        double area;
+        if (str == "1") {
+            area = EquilateralTriangleMenu();
+        }
+        else if (str == "2") {
+            area = VersatileTriangleMenu();
+        }
+        std::cout << "Площадь треугольник =" << area << std::endl;
+    }
 }
