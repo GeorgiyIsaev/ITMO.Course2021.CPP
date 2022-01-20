93600fd6e98d81c24a04a3f9bdc390c88646e811
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-20 22:06:23 +0300

    Квадратное уравние

--- Lab04.PointersAndLinks/Lab04.PointersAndLinks.cpp
+++ Lab04.PointersAndLinks/Lab04.PointersAndLinks.cpp
@@ -1,5 +1,6 @@
 ﻿#include <windows.h>
 #include "Lab04.h"
+#include "Task01.SqrtTwo.h"
 
 int main()
 {
@@ -7,11 +8,11 @@ int main()
 	SetConsoleCP(1251);
 
 	/*Практическое занятие 4. Использование указателей и ссылок*/
-	Lab04Main();
+	//Lab04Main();
 
 	/*Контрольные задания*/
 	/*Задание 1. Вычисление корней квадратного уравнения*/
-
+	MainSquareRoot();
 
 
 }
--- Lab04.PointersAndLinks/Task01.SqrtTwo.h
+++ Lab04.PointersAndLinks/Task01.SqrtTwo.h
@@ -13,7 +13,54 @@
 int Myroot(double, double, double, double&, double&);*/
 
 
+int Myroot(double, double, double, double&, double&);
+std::string SquareRootStr(double a, double b, double c) {
+	std::string text = std::to_string(a) + "x^2 + "+
+		std::to_string(b) +"x + "+ std::to_string(c) +" = 0";
+	return text;
+}
+
 void MainSquareRoot() {
 
 
+	std::cout << "Значения для квадратного уравнения ax^2+bx+c=0" << std::endl;
+	double a = GetValue("a= ");
+	double	b = GetValue("b= ");
+	double	c = GetValue("c= ");
+	std::cout << "Уравнения " << SquareRootStr(a,b,c) << std::endl;
+
+	double	x1, x2; //корни
+
+	int result = Myroot(a, b, c, x1, x2);
+	switch (result)
+	{
+	case 1:
+		std::cout << "x1 = " << x1 << "\nx2 = " << x2 << std::endl;
+		break;
+	case 0:
+		std::cout << "x1 = x2 = " << x1 << std::endl;
+		break;
+	case (-1):
+		std::cout << "У данного уравнения корней нет.";
+		break;
+	}	
+}
+
+int Myroot(double a, double b, double c, double& x1, double& x2)
+{
+	double D = b * b - 4 * a * c;
+	if (D > 0)
+	{
+		x1 = ((-1) * b + sqrt(D)) / (2 * a);
+		x2 = ((-1) * b - sqrt(D)) / (2 * a);;
+		return 1;
+	}
+	else if (D == 0)
+	{
+		x1 = (-1) * b / (2 * a);
+		x2 = x1;
+		return 0;
+	}
+	else
+		return -1;
 }
