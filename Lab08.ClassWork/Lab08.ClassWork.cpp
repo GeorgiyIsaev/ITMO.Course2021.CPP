907326a1504945ff0c706abb3be8bbeebc8e58f9
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-23 01:44:40 +0300

    мда

--- Lab08.ClassWork/Lab08.ClassWork.cpp
+++ Lab08.ClassWork/Lab08.ClassWork.cpp
@@ -1,5 +1,6 @@
 ﻿#include "MyInclude.h"
 #include "Lab08.Student.h"
+#include "Lab08.StudentCPP.h"
 
 int main()
 {
@@ -9,4 +10,7 @@ int main()
     Реализация инкапсуляции. Конструкторы и деструкторы.
     /*Упражнение 1. Реализация сущности – студент в виде класса*/
     // MainStudent();
+
+    /*Упражнение 2. Разделение реализации и представления*/
+    //MainStudentHCPP();
 }
