aee1dd69fcc67bac73b9940f4dfb354b71ff020b
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-23 01:18:00 +0300

    task03 распаковка

--- Lab07.StructWork/Task03.SqrtRootX.h
+++ Lab07.StructWork/Task03.SqrtRootX.h
@@ -13,7 +13,60 @@
 решение этого уравнения (корни уравнения) и флаг, 
 показывающий наличие корней.*/
 
+std::string SquareRootStr(double a, double b, double c) {
+	std::string text = std::to_string(a) + "x^2 + " +
+		std::to_string(b) + "x + " + std::to_string(c) + " = 0";
+	return text;
+}
+
+std::tuple<int, double, double> RootSqrt(double a, double b, double c)
+{
+	double x1, double x2;
+	double D = b * b - 4 * a * c;
+	int flag;
+	if (D > 0)
+	{
+		x1 = ((-1) * b + sqrt(D)) / (2 * a);
+		x2 = ((-1) * b - sqrt(D)) / (2 * a);
+		flag = 1;
+	}
+	else if (D == 0)
+	{
+		x1 = (-1) * b / (2 * a);
+		x2 = x1;	
+		flag = 0;
+	}
+	else {
+		flag = -1;
+		x2 = x1 = 0.0;
+	}
+	return std::make_tuple(flag, x1, x2);
+		
+}
 
 void MainTupleSqrtRoot() {
+	std::cout << "Значения для квадратного уравнения ax^2+bx+c=0" << std::endl;
+	double a = GetValue("a= ");
+	double	b = GetValue("b= ");
+	double	c = GetValue("c= ");
+	std::cout << "Уравнения: " << SquareRootStr(a, b, c) << std::endl;
 
+	double	x1, x2; //корни
+
+	//std::tie(a, b) -  распкаовавыет кортеж в переменные ();
+	std::tuple<int, double, double> result = RootSqrt(a, b, c);
+	int flag = result.get<1>();
+	int flag = result.get<1>();
+	switch ()
+	{
+	case 1:
+		std::cout << "x1 = " << x1 << "\nx2 = " << x2 << std::endl;
+		break;
+	case 0:
+		std::cout << "x1 = x2 = " << x1 << std::endl;
+		break;
+	case (-1):
+		std::cout << "У данного уравнения корней нет.";
+		break;
+	}
 }
