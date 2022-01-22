c43877185db6c37f655fb3e6b3288d7a2418f003
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-23 00:20:14 +0300

    struct Time

--- Lab07.StructWork/Task01.Time.h
+++ Lab07.StructWork/Task01.Time.h
@@ -1,10 +1,12 @@
 #pragma once
+#include "MyInclude.h"
 
 /*Задание 1. Структура Time
 Создайте структуру с именем Time. Три ее поля, имеющие
-тип int, назовите hours, minutes и seconds. Напишите программу,
-которая просит пользователя ввести время в формате часы, минуты,
-секунды. Можно запрашивать на ввод как три значения сразу,
+тип int, назовите hours, minutes и seconds.
+Напишите программу, которая просит пользователя ввести время
+в формате часы, минуты, секунды. 
+Можно запрашивать на ввод как три значения сразу,
 так и выводить для каждой величины отдельное приглашение.
 Программа должна хранить время в структурной переменной
 типа Time и выводить количество секунд во введенном времени.
@@ -14,5 +16,35 @@
 значения для реализуемых характеристик.*/
 
 struct Time {
-
+    int hours = 0;
+    int minutes = 0;
+    int seconds = 0;
+    void SetTime(int hours, int minutes, int seconds) {
+       this->hours = hours; 
+       this->minutes = minutes;
+       this->seconds = seconds;
+    }
+    void CinTime() {
+        this->hours = GetValue("Введите hours: ");
+        this->minutes = GetValue("Введите minutes: ");
+        this->seconds = GetValue("Введите seconds: ");
+    }
+    void RecountTime() {
+        while (seconds > 60) {
+            seconds = -60;
+            minutes++;
+        }
+        while (minutes > 60) {
+            minutes = -60;
+            hours++;
+        }
+    }
+    std::string GetStrTime() {
+        "{10h;2m;10s}";
+        std::string text = "{" +
+            std::to_string(hours) + "h;" +
+            std::to_string(minutes) + "m;" +
+            std::to_string(seconds) + "s}";
+        return text;
+    }
 };
