6e6baed589abfa3c5bf4824f2aba6ba31a8cf371
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-24 23:01:43 +0300

    create LAB13

---
+++ Lab13.Polymorphism/Lab13.Polymorphism.cpp
@@ -0,0 +1,11 @@
+﻿#include "MyInclude.h"
+#include "Lab13.Main.h"
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	/*Практическое занятие 13. Применение полиморфизма*/
+	MainLab13();
+}
+
