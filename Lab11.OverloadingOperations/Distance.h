74d5030d4b11d30f37e8af26427ab3e40af61580
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-24 18:50:07 +0300

    Оперторы

---
+++ Lab11.OverloadingOperations/Distance.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "MyInclude.h"
+
+class Distance
+{
+private:
+	int feet;
+	float inches;
+public:
+	// конструктор по умолчанию
+	Distance() : feet(0), inches(0.0) { }
+	// конструктор с двум€ параметрами
+	Distance(int ft, float in) : feet(ft), inches(in) { }
+	void getdist()
+	{
+		std::cout << "\n¬ведите число футов : ";
+		std::cin >> feet;
+		std::cout << "\n¬ведите число дюймов : ";
+		std::cin >> inches;
+	}
+	void showdist()
+	{
+		std::cout << feet << "\" - " << inches << "\"\n";
+	}
+	Distance operator+ (const Distance& d2) const
+	{
+		int f = feet + d2.feet;
+		float i = inches + d2.inches;
+		if (i >= 12.0)
+		{
+			i -= 12.0;
+			f++;
+		}
+		return Distance(f, i);
+	}
+	Distance operator- (const Distance& d2) const
+	{
+		int f = feet - d2.feet;
+		float i = inches - d2.inches;
+		if (i <= 0.0)
+		{
+			i += 12.0;
+			f--;
+		}
+		return Distance(f, i);
+	}
+};
--- Lab11.OverloadingOperations/Lab11.Main.h
+++ Lab11.OverloadingOperations/Lab11.Main.h
@@ -1,7 +1,28 @@
 #pragma once
+#include "Distance.h"
+
 
 
 void MainLab11() {
 	/*Практическое занятие 11. Перегрузка операций
 	/*Упражнение 1. Перегрузка бинарных операций*/
+	Distance dist1, dist2, dist3, dist4;
+
+	dist1.getdist();
+	dist2.getdist();
+	dist3.getdist();
+	dist4.getdist();
+	dist3 = dist1 + dist2;
+	dist4 = dist1 + dist2 + dist3;
+	std::cout << "\ndist1 = ";
+	dist1.showdist();
+	std::cout << "\ndist2 = ";
+	dist2.showdist();
+	std::cout << "\ndist3 = ";
+	dist3.showdist();
+	std::cout << "\ndist4 = ";
+	dist4.showdist();
+	dist3 = dist1 - dist2;
+	std::cout << "\ndist3 = ";
+	dist3.showdist();
 }
