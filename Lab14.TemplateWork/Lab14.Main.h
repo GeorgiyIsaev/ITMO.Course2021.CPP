bdb4a19816d9864d1d467d12b4dbe11f09eabeeb
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-25 23:47:26 +0300

    lab14 pr3

--- Lab14.TemplateWork/Lab14.Main.h
+++ Lab14.TemplateWork/Lab14.Main.h
@@ -68,6 +68,44 @@ void Task02() {
 	printTupleOfThree(t3);
 }
 
+
+/*Упражнение 3. Использование шаблонной функции для работы
+	с кортежем любого размера*/
+template<class Tuple, std::size_t N>
+struct TuplePrinter {
+	static void print(const Tuple& t)
+	{
+		TuplePrinter<Tuple, N - 1>::print(t);
+		std::cout << ", " << std::get<N - 1>(t);
+	}
+};
+template<class Tuple>
+struct TuplePrinter<Tuple, 1> {
+	static void print(const Tuple& t)
+	{
+		std::cout << std::get<0>(t);
+	}
+};
+template<class... Args>
+void printTuple(const std::tuple<Args...>& t)
+{
+	std::cout << "(";
+	TuplePrinter<decltype(t), sizeof...(Args)>::print(t);
+	std::cout << ")" << std::endl;
+}
+void Task03() {
+	/*Упражнение 3. Использование шаблонной функции для работы
+	с кортежем любого размера*/
+	std::vector<std::string> v1{ "one", "two", "three", "four", "five", "six" };
+	std::vector<int> v2 = { 1, 2, 3, 4, 5, 6 };
+	std::vector<float> v3 = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+	auto t1 = std::make_tuple(v1[0], v1[1], v3[0]);
+	auto t2 = std::make_tuple(v1[0], v1[1], v2[1], v3[0], v3[1]);
+	printTuple(t1);
+	printTuple(t2);
+
+}
+
 void MainLab14() {
 	/*Практическое занятие 14. Использование шаблонных функций и классов*/
 	/*Упражнение 1. Создание шаблонной функции сортировки массива*/
@@ -77,5 +115,8 @@ void MainLab14() {
 	/*Упражнение 2. Создание шаблонной функции для работы с кортежем*/
 	Task02();
 
+	/*Упражнение 3. Использование шаблонной функции для работы
+	с кортежем любого размера*/
+	Task03();
 }
 
