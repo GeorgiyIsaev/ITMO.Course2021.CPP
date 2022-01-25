f1f1c3a1b49763f30257039d3da85a9f50f32f48
Георгий Исаев <georgiyelbaf@gmail.com>
2022-01-26 01:45:28 +0300

    Lab14 Task01 Full

--- Lab14.TemplateWork/Task01.MainTemplateArray.h
+++ Lab14.TemplateWork/Task01.MainTemplateArray.h
@@ -8,21 +8,60 @@
 Аргументами функции должны быть имя и размер массива (типа int).
 В функции main() проверьте работу с массивами типа int, long, double и char.*/
 
-
-int Averge(int size, int* array) {
-	int sum=0;
+template<class T>
+T Averge(int size, T* array) {
+	T sum=0;
 	for (int i = 0; i < size; i++) {
 		sum += array[i];
 	}
 	return sum / size;
 }
 
-void MainTemplateArray() {
+template<class T>
+std::string StrArr(int size, T* array) {
+	std::ostringstream ost;
+	ost << std::fixed << std::setprecision(1) <<"[";
+	for (int i = 0; i < size; i++) {
+		ost << array[i] << ";";
+	}
+	ost << "]";
+	return ost.str();
+}
+
+void MainTemplateArray() {	
 	int size = 10;
+	std::cout << "Массив Int:\n";
 	int* arrInt = new int[size];
-
-
-	arrInt = { 1,2,3,4,5,6,7,8,9,0 };
-
+	for (int i = 0; i < size; i++)
+		arrInt[i] = (rand() % 10 + 1);
+	std::cout << StrArr(size, arrInt) << std::endl;
+	std::cout << "Среднее Арифмитическое: " << Averge(size, arrInt) << std::endl;
 	delete[]arrInt;
+
+
+	std::cout << "\nМассив long:\n";
+	long* arrLong = new long[size];
+	for (int i = 0; i < size; i++)
+		arrLong[i] = (rand() % 100000000000 + 1);
+	std::cout << StrArr(size, arrLong) << std::endl;
+	std::cout << "Среднее Арифмитическое: " << Averge(size, arrLong) << std::endl;
+	delete[]arrLong;
+
+	std::cout << "\nМассив Double:\n";
+	double* arrDouble = new double[size];
+	for (int i = 0; i < size; i++)
+		arrDouble[i] = (rand() % 10 + 1) / double((rand() % 10 + 1));
+	std::cout << StrArr(size, arrDouble) << std::endl;
+	std::cout << "Среднее Арифмитическое: " << Averge(size, arrDouble) << std::endl;
+	delete[]arrDouble;
+
+	std::cout << "\nМассив char:\n";
+	char* arrChar = new char[size];
+	for (int i = 0; i < size; i++)
+		arrChar[i] = (char)(rand() % 200 + 1);
+	std::cout << StrArr(size, arrChar) << std::endl;
+	std::cout << "Среднее Арифмитическое: " << Averge(size, arrChar) << std::endl;
+	delete[]arrChar;
+
+
 }
