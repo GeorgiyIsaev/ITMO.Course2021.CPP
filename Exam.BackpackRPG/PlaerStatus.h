e84e8b31d46415f25134c35cafa1169ea7f08980
Георгий Исаев <georgiyelbaf@gmail.com>
2022-02-01 18:15:14 +0300

    Статусбар

---
+++ Exam.BackpackRPG/PlaerStatus.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "MyInclude.h"
+
+
+
+class PlaerStatus {
+private:
+	std::string name;
+	int heat;
+	int strength;	
+	int dotX;
+	int dotY;
+public:
+	PlaerStatus() {
+		name = "Plaer";
+		heat = 100;
+		strength = 3;
+		dotX = 5; dotY = 5;
+	}
+	std::string StatusBar() {
+		return name + "; HP= " + std::to_string(heat) +
+			"; STR= " + std::to_string(strength) + 
+			" [" + std::to_string(dotX) + ":"+ std::to_string(dotY) + "]";
+	}
+};
+
