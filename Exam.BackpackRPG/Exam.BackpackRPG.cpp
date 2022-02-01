b62cfe88d0f8f48aff0fea64cfc897e3075934f4
Георгий Исаев <georgiyelbaf@gmail.com>
2022-02-01 18:05:59 +0300

    Тест вынесен в отдельный файл

--- Exam.BackpackRPG/Exam.BackpackRPG.cpp
+++ Exam.BackpackRPG/Exam.BackpackRPG.cpp
@@ -2,6 +2,7 @@
 #include "Backpack.h"
 #include "WeaponOff.h"
 #include "ItemDefOff.h"
+#include "Test.h"
 
 /*ИТОГОВОЕ задание*/
 /*Требования*/
@@ -16,32 +17,7 @@ int main()
 	SetConsoleCP(1251);
 	srand(time(0));
 
-	Backpack backpack;
-	backpack.AddItem(WeaponOff::createWeapon(1));
-	backpack.AddItem(WeaponOff::createWeapon(3));
-	backpack.AddItem(WeaponOff::createWeapon(4));
-	backpack.AddItem(RunaDefOff::createRuna(3));
-	backpack.PrintBackpack();
-
-	backpack.AddItem(ItemDefOff::createItem("WeaponDef;;;Нож;;;нож;;;99"));
-	backpack.PrintBackpack();
-	backpack.FileSave();
-
-	backpack.ReadFile();
-	backpack.PrintBackpack();
-
-
-	for (int i = 0; i < 7; i++) {
-		backpack.AddItem(ItemDefOff::createRandomItem());
-	}
-	backpack.PrintBackpack();
-	backpack.FileSave();
-
-	for (int i = 0; i < 5; i++) {
-		int val = GetRandomInt(0, backpack.Size());
-		backpack.UseItem(val);
-	}
-
+	//TestBackpack(); //тест сумки
 
 }
 
---
+++ Exam.BackpackRPG/Test.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "MyInclude.h"
+#include "Backpack.h"
+#include "WeaponOff.h"
+#include "ItemDefOff.h"
+
+void TestBackpack() {
+	Backpack backpack;
+	backpack.AddItem(WeaponOff::createWeapon(1));
+	backpack.AddItem(WeaponOff::createWeapon(3));
+	backpack.AddItem(WeaponOff::createWeapon(4));
+	backpack.AddItem(RunaDefOff::createRuna(3));
+	backpack.PrintBackpack();
+
+	backpack.AddItem(ItemDefOff::createItem("WeaponDef;;;Нож;;;нож;;;99"));
+	backpack.PrintBackpack();
+	backpack.FileSave();
+
+	backpack.ReadFile();
+	backpack.PrintBackpack();
+
+
+	for (int i = 0; i < 7; i++) {
+		backpack.AddItem(ItemDefOff::createRandomItem());
+	}
+	backpack.PrintBackpack();
+	backpack.FileSave();
+
+	for (int i = 0; i < 5; i++) {
+		int val = GetRandomInt(0, backpack.Size());
+		backpack.UseItem(val);
+	}
+
+}
